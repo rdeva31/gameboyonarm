9187049d1c9b97dccdb506de44ece9f8a7deff54
r.deva31 <r.deva31@af2ec6d8-0484-11df-adb7-1be7f7e3f636>
2010-03-17 16:13:43 +0000

    gfx_init works perfectly (in frame buffer mode), we might need to move away from this mode later on
    gfx_draw() has some example crap in it

--- trunk/source/Graphics.c
+++ trunk/source/Graphics.c
@@ -1,10 +1,12 @@
 #include "Base.h"
 
 #include "Graphics.h"
-
-void gfx_draw(u8 bytes[])
+void gfx_draw()
 {
+	int i;
 	
+	for(i=0; i < 20057; ++i)
+		VRAM_A[i] = i & 0xff;
 }
 
 /*
@@ -21,7 +23,7 @@ void gfx_init()
 																			//, which is controlled by bits 16 and 17
 	*DISPLAY_CONTROL_REG = (*DISPLAY_CONTROL_REG & ~(3<<18)) | BITMAP_DATA_BLOCK;	//in bitmap_mode 2 we can specify which block
 																					//to read data from, which is bits 18 and 19
-																					
+	
 	//map VRAM A to a specific physical memory
-	*VRAM_A_CONTROL_REG = VRAM_A_ENABLE | VRAM_A_OFFSET | VRAM_A_MST;
+	*VRAM_A_CONTROL_REG = VRAM_A_ENABLE | VRAM_A_OFFSET | VRAM_A_MST;																		
 }
