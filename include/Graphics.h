2fa434672f9e826eafc8bf27023214bf06ed6d2c
r.deva31 <r.deva31@af2ec6d8-0484-11df-adb7-1be7f7e3f636>
2010-03-17 01:20:57 +0000

    Created Graphics.c and Graphics.h and #define'd relevant definitions

---
+++ include/Graphics.h
@@ -0,0 +1,32 @@
+#ifndef GRAPHICS_H
+#define GRAPHICS_H
+
+/* References to specific registers can be found here: 
+	http://nocash.emubase.de/gbatek.htm#dstechnicaldata
+*/
+
+// #defines pertaining to controlling the engines
+#define DISPLAY_CONTROL_REG ((unsigned int *)0x4000000)	/* Register DISPCNT, controls the modes of each engine */
+#define BITMAP_MODE (2<<16)	/* To be used to set DISPLAY_CONTROL_REG to a basic bitmap mode */
+#define BITMAP_DATA_BLOCK (0<<18)	/* To be used iff BITMAP_MODE == 2<<16. This specifies, which block
+										contains graphics.  The value of this can be between [0,4] */
+							
+// #defines pertaining to mapping the VRAM A bank to memory
+#define VRAM_A_CONTROL_REG ((unsigned int *) 0x4000240) /* Register VRAMCNT_A, which specifies the logical mapping
+															to physical mapping of VRAM_A bank. Bit map of VRAMCNT_A:
+															0-2   VRAM MST              ;Bit2 not used by VRAM-A,B,H,I
+															3-4   VRAM Offset (0-3)     ;Offset not used by VRAM-E,H,I
+															5-6   Not used
+															7     VRAM Enable (0=Disable, 1=Enable)
+														*/
+#define VRAM_A_MST 0	/* Choose memory range 0x6800000 to 0x681FFFF */
+#define VRAM_A_OFFSET (0<<3)	/* since VRAM_MST_0 == 0, no need for offset */
+#define VRAM_A_ENABLE 1 /* Well duh, we want it enabled so it shows stuff */
+#define VRAM_A ((unsigned int *)0x0x06800000) /* Pointer to a segment of memory that is mapped to by VRAM_A_MST and VRAM_A_OFFSET */
+
+
+
+//function prototypes... see Graphics.c for documentation
+void gfx_draw(u8 bytes[]);
+void gfx_init();
+#endif
---
+++ source/Graphics.c
@@ -0,0 +1,27 @@
+#include "Base.h"
+#include "Graphics.h"
+
+void gfx_draw(u8 bytes[])
+{
+	
+}
+
+/*
+	Initalizes the screen by setting the top and bottom screens to appropriate modes.
+	Call this before invoking other gfx_* functions.
+	
+	This might interfere with other procedures that utilize the screen.
+*/
+void gfx_init()
+{
+
+	//set up the display
+	*DISPLAY_CONTROL_REG = (*DISPLAY_CONTROL_REG & ~(3<<16)) | BITMAP_MODE; //sets the mode of Engine A to bitmap mode
+																			//, which is controlled by bits 16 and 17
+	*DISPLAY_CONTROL_REG = (*DISPLAY_CONTROL_REG & ~(3<<18)) | BITMAP_DATA_BLOCK;	//in bitmap_mode 2 we can specify which block
+																					//to read data from, which is bits 18 and 19
+																					
+	//map VRAM A to a specific physical memory
+	*VRAM_A_CONTROL_REG = 0;
+	*VRAM_A_CONTROL_REG = VRAM_A_ENABLE | VRAM_A_OFFSET | VRAM_A_MST;
+}
