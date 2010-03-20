893fb4aa0dec06b14c0bfaf1dc9a2445b6a1ae5b
r.deva31 <r.deva31@af2ec6d8-0484-11df-adb7-1be7f7e3f636>
2010-03-20 01:14:44 +0000

    Nothing major.

--- trunk/source/Main.c
+++ trunk/source/Main.c
@@ -1,9 +1,8 @@
-#include "Base.h"
-
-#include "Cartridge.h"
-#include "Graphics.h"
-#include "Memory.h"
-
+#include <Base.h>
+#include <Cartridge.h>
+#include <Graphics.h>
+#include <Memory.h>
+#include <stdio.h>
 extern void executeFrame(); //TODO: Put this in a processor.h later
 
 int main()
@@ -15,6 +14,6 @@ int main()
 	executeFrame();
 	
 	gfx_init();
-	gfx_draw();
+	gfx_draw(NULL);
 	return 0;
 }
