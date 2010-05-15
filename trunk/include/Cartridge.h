1ecd825ed3c6ad27652dee165be958116f53474c
r.deva31 <r.deva31@af2ec6d8-0484-11df-adb7-1be7f7e3f636>
2010-05-15 23:53:13 +0000

    Added cartridge read functionality

--- trunk/include/Cartridge.h
+++ trunk/include/Cartridge.h
@@ -3,6 +3,17 @@
 
 #include <Base.h>
 
-void cartridgeWrite(u16 address, u8 value);
+typedef struct
+{
+	u8 type;
+	u8 rom_size;
+	u8 ram_size;
+} ctg_info;
+
+
+#define DEFAULT_FILE "/TETRIS.GB"
+
+void ctg_read_bank(ctg_info, u8);
+void ctg_init(ctg_info *);
 
 #endif
