b8654eaf73777c96cd98e2e83aad82542a13ca11
mlentz743 <mlentz743@af2ec6d8-0484-11df-adb7-1be7f7e3f636>
2010-05-18 04:15:33 +0000

    Few more changes

--- source/Main.c
+++ source/Main.c
@@ -4,8 +4,7 @@
 #include <Memory.h>
 #include <stdio.h>
 
-extern void executeFrame(); //TODO: Put this in a processor.h later
-
+extern void executeFrame();
 extern volatile u8 RAM[64 * 1024];
 extern volatile u32 ProcessorState[8];
 
@@ -23,17 +22,34 @@ int main()
 
 	printf("Graphics Initialized...\n");
 	
+	//Initializing special registers in RAM
+	RAM[0xFF05] = 0x00;
+	RAM[0xFF06] = 0x00;
+	RAM[0xFF07] = 0x00;
+	RAM[0xFF40] = 0x91;
+	RAM[0xFF42] = 0x00;
+	RAM[0xFF43] = 0x00;
+	RAM[0xFF45] = 0x00;
+	RAM[0xFF47] = 0xFC;
+	RAM[0xFFF8] = 0xFF;
+	RAM[0xFFF9] = 0xFF;
+	RAM[0xFFFA] = 0x00;
+	RAM[0xFF4B] = 0x00;
+	RAM[0xFFFF] = 0x00;
+
 	while(1)
 	{
 		executeFrame();
 	
-		//printf("Registers:\n");
-		//printf("\tAF = 0x%04X\n", ProcessorState[0] & 0xFFFF);
-		//printf("\tBC = 0x%04X\n", (ProcessorState[0] >> 16) & 0xFFFF);
-		//printf("\tDE = 0x%04X\n", (ProcessorState[1] >> 16) & 0xFFFF);
-		//printf("\tHL = 0x%04X\n", ProcessorState[1] & 0xFFFF);
-		//printf("\tSP = 0x%04X\n", ProcessorState[2]);
-		//printf("\tPC = 0x%04X\n", ProcessorState[3]);
+		/*
+		printf("Registers:\n");
+		printf("\tAF = 0x%04X\n", ProcessorState[0] & 0xFFFF);
+		printf("\tBC = 0x%04X\n", (ProcessorState[0] >> 16) & 0xFFFF);
+		printf("\tDE = 0x%04X\n", (ProcessorState[1] >> 16) & 0xFFFF);
+		printf("\tHL = 0x%04X\n", ProcessorState[1] & 0xFFFF);
+		printf("\tSP = 0x%04X\n", ProcessorState[2]);
+		printf("\tPC = 0x%04X\n", ProcessorState[3]);
+		*/
 		
 		gfx_canvas_info i;
 		gfx_setup(&i);
